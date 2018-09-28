c7803260bc301b8cba93a37f841fba2560bc5eef
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-09-28 13:05:36 -0400

    Updated from Brython Server: 9/28/2018 1:05:34 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,8 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Jackson Lake
+Credit: https://www.youtube.com/watch?v=u1jdar3WADY
+https://www.youtube.com/watch?v=OFSELeMx2nE
 
 Assignment:
 
@@ -25,3 +26,13 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+string = input("Please enter a string of text (the bigger the better):")
+print(string[::-1])
+
+sep = string.split() 
+print(' '.join((sep[::-1])))
+
+one = (string[::-1])
+three = one.split()
+print(' '.join(three[::-1]))
+
