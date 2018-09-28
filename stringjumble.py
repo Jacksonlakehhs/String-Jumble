1e7cb30474576d788a51c5579a0483d80a02dd9a
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-09-28 13:08:27 -0400

    Updated from Brython Server: 9/28/2018 1:08:25 PM

--- stringjumble.py
+++ stringjumble.py
@@ -27,6 +27,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 string = input("Please enter a string of text (the bigger the better):")
+input("You entered ""{0}"" Now jumble it:"
 print(string[::-1])
 
 sep = string.split() 
