ffcf563eb532c7a47a036cc32bf0413b1c3fafca
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-09-29 15:56:19 -0400

    Updated from Brython Server: 9/29/2018 3:56:18 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,8 +26,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-string = input("Please enter a string of text (the bigger the better):")
-input("You entered ""{0}"" Now jumble it:".format(string))
+string = input("Please enter a string of text (the bigger the better): ")
+print('You entered "{0}". Now jumble it:'.format(string))
 print(string[::-1])
 
 sep = string.split() 
@@ -36,4 +36,3 @@ print(' '.join((sep[::-1])))
 one = (string[::-1])
 three = one.split()
 print(' '.join(three[::-1]))
-
